f0d662011175966eb6d391baddadb31492843d9e
Cai Gao <cai.gao@icloud.com>
2014-04-01 11:43:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuperHeroes::Application.config.secret_key_base = '85ed6fb09f1e81daa057f381af1dda94be57f5354c83d4d0d5f44b0dd9ed78b791d9ea57b84a95c0a85c99cb17554dd5091a7ea89209cffcfc27e0375942e334'
