3fe289d0b538a07cd1942916d18e085ff656dc4b
Cai Gao <cai.gao@icloud.com>
2014-04-02 16:04:57 -0400

    Complete User Profile Edit

--- spec/features/user_edits_their_profile.rb
+++ spec/features/user_edits_their_profile.rb
@@ -16,15 +16,33 @@ feature 'user edits their profile', %q{
 # *I receive an error message if I provided an invalid email
 # *I receive an error message if my email is not unique
 # *I receive an error message if my password does not match up
+  before :each do
+    @user = FactoryGirl.create(:user)
+    sign_in_as @user
+  end
 
   scenario 'user can access an edit page' do
-    user = FactoryGirl.create(:user)
-    sign_in_as user
-
-    visit root_path
     click_on 'Edit Profile'
     expect(page).to have_content('Edit User')
   end
-  scenario 'user can change their email'
-  scenario 'user can change their password'
+
+  scenario 'user can change their email' do
+    click_on 'Edit Profile'
+    fill_in 'Email', with: 'superduper@e.com'
+    fill_in 'Current password', with: @user.password
+    click_on 'Update'
+
+    expect(User.where(username: @user.username).take.email).to eq('superduper@e.com')
+    expect(page).to have_content('You updated your account successfully.')
+  end
+
+  scenario 'user can change their password' do
+    click_on 'Edit Profile'
+    fill_in 'Password', with: 'password2'
+    fill_in 'Password confirmation', with: 'password2'
+    fill_in 'Current password', with: @user.password
+    click_on 'Update'
+
+    expect(page).to have_content('You updated your account successfully.')
+  end
 end
