35c23e849a85f29e59631f0d9cde55d25734c7cf
Gregory Sheppard <gregory.sheppard@live.com>
2014-04-07 11:09:45 -0400

    Added user_id to movies table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,7 @@ class Movie < ActiveRecord::Base
   validates :mpaa_rating, inclusion: {in: ["G", "PG", "PG-13", "R", "NC-17", "", nil]}
 
   has_many :reviews
+  belongs_to :user
 
   def average_rating
     if reviews.count > 0
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402160456) do
+ActiveRecord::Schema.define(version: 20140407150213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(version: 20140402160456) do
     t.string   "director"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "movies", ["title", "year"], name: "index_movies_on_title_and_year", unique: true, using: :btree
+  add_index "movies", ["user_id"], name: "index_movies_on_user_id", using: :btree
 
   create_table "reviews", force: true do |t|
     t.integer  "movie_id",   null: false
@@ -38,19 +40,20 @@ ActiveRecord::Schema.define(version: 20140402160456) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                            null: false
+    t.string   "username",                               null: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
