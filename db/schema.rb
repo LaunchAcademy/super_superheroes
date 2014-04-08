2dbbe3a59842b4ccc66e5cba048e01dc6c14e840
Helen Hood <helen.c.hood@gmail.com>
2014-04-08 13:57:34 -0400

    Add destroy dependency to Movie for has_many :reviews

--- db/schema.rb
+++ db/schema.rb
@@ -40,20 +40,19 @@ ActiveRecord::Schema.define(version: 20140407150213) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                               null: false
-    t.boolean  "admin",                  default: false, null: false
+    t.string   "username",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
