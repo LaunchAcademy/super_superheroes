9e525f7d21c9d968869a1e0fb44fdc8f40165e48
Cai Gao <cai.gao@icloud.com>
2014-04-08 12:04:24 -0400

    Removed Admin Movies views and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,6 @@ class ApplicationController < ActionController::Base
 
   def admin?
     true if current_user.role == 'admin'
-    # if !user_signed_in?
-    #   redirect_to new_user_session_path, alert: 'Please sign in or sign up to continue.'
-    # elsif !(current_user.role == 'admin')
-    #   redirect_to root_path, alert: 'Unauthorized access.'
-    # end
   end
 
 end
