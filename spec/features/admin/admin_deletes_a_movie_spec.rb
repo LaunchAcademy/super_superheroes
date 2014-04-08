3b738acd9daf025fdf219d7bb7da0286dc9493f2
Cai Gao <cai.gao@icloud.com>
2014-04-08 16:52:43 -0400

    Updated admin_deletes_movie to allow deletion on the show page

--- spec/features/admin/admin_deletes_a_movie_spec.rb
+++ spec/features/admin/admin_deletes_a_movie_spec.rb
@@ -11,31 +11,44 @@ feature 'admin deletes a movie', %Q{
   # * I can click on a link next to each movie to delete it
   # * When I delete a movie, its reviews and votes are also deleted
 
+  let!(:movies) { FactoryGirl.create_list(:movie, 3) }
+  let!(:review) { FactoryGirl.create_list(:review, 3, movie: movies.first) }
+
   context 'logged in as admin' do
     before :each do
       @admin = FactoryGirl.create(:user, role: 'admin')
-      @movies = FactoryGirl.create_list(:movie, 3)
-      @review = FactoryGirl.create_list(:review, 3, movie: @movies.first)
       @prev_count = Movie.count
       sign_in_as(@admin)
       visit movies_path
     end
 
     scenario 'Admin can see list of movies' do
-      @movies.each do |movie|
+      movies.each do |movie|
         expect(page).to have_content(movie.title)
       end
     end
 
-    scenario 'Admin delete movie' do
-
-      within(:css, "#movie_#{@movies.first.id}") do
+    scenario 'Admin delete movie from index' do
+      within(:css, "#movie_#{movies.first.id}") do
         click_link 'Delete'
       end
 
       expect(Movie.count).to eq(@prev_count - 1)
-      expect(page).to_not have_content(@movies.first.title)
-      expect(@movies.first.reviews.count).to eq(0)
+      expect(page).to_not have_content(movies.first.title)
+      expect(movies.first.reviews.count).to eq(0)
+    end
+
+    scenario 'Admin deletes movie from show page' do
+      visit movie_path(movies[0])
+
+      within(:css, '.movie') do
+        expect(page).to have_link('Delete')
+      end
+
+      click_link 'Delete'
+
+      expect(current_path).to eq(movies_path)
+      expect(Movie.count).to eq(movies.count - 1)
     end
 
   end
@@ -47,6 +60,16 @@ feature 'admin deletes a movie', %Q{
       visit movies_path
 
       expect(page).to_not have_content('Delete')
+    end
+
+    scenario 'visit single movie page as non-admin' do
+      user = FactoryGirl.create(:user)
+      sign_in_as(user)
+      visit movie_path(movies[0])
+
+      within(:css, '.movie') do
+        expect(page).to_not have_link('Delete')
+      end
     end
   end
 
