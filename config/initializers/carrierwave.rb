7745381a86d623dd9adf144aba9b924e08cb663e
Cai Gao <cai.gao@icloud.com>
2014-04-24 11:54:01 -0400

    change access key names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['aws_access_key'],
-    aws_secret_access_key: ENV['aws_secret_key']
+    aws_access_key_id: ENV['aws_access_key_id'],
+    aws_secret_access_key: ENV['aws_secret_access_key']
   }
   config.fog_directory  = "Superheroes_grp_project"
 end
