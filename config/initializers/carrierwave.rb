9f2ed166c6c869f7c73e677db7fb60c43ff852b7
Cai Gao <cai.gao@icloud.com>
2014-04-24 12:10:28 -0400

    change access key to cap

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['aws_access_key_id'],
-    aws_secret_access_key: ENV['aws_secret_access_key']
+    AWS_ACCESS_KEY_ID: ENV['AWS_ACCESS_KEY_ID'],
+    AWS_SECRET_ACCESS_KEY: ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = "Superheroes_grp_project"
 end
